03a48214d7ae57440f9b5832d2a55a1381a16db2
MynorEscobar <mynorescobarefpem@gmail.com>
2022-03-30 19:22:36 -0600

    NuevoActivity2.kt agregado

---
+++ app/src/main/java/com/example/ejemplorecyclerview/NuevoActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.ejemplorecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NuevoActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nuevo2)
+    }
+}
